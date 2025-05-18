5af81321beca8a633da0ed8c341ae62b625e3283
marvelousstorm <brian_vinn@hotmail.com>
2025-05-18 14:30:53 -0600

    finish CRUD tests

--- cypress/e2e/smokeUI/CRUD.cy.js
+++ cypress/e2e/smokeUI/CRUD.cy.js
@@ -9,7 +9,7 @@ let firstName, lastName, dependants
 describe('CRUD test', { tags: ['@smoke'], cases: [1] }, () => {
     before(() => {
         cy.fixture('test_data.json').then((test_data) => {
-            let testD = (env === 'stage') ? test_data.stage : test_data.production.CRUD
+            let testD = (env === 'stage') ? test_data.stage.CRUD : test_data.production.CRUD
             firstName = testD.employee.firstName
             lastName = testD.employee.lastName
             dependants = testD.employee.dependants
@@ -19,14 +19,14 @@ describe('CRUD test', { tags: ['@smoke'], cases: [1] }, () => {
         cy.login(URL, username, password)
     })
 
-    it('Create employee', { cases: [1] }, () => {
+    it('Create employee', { cases: [2] }, () => {
         homePage.getAddEmployeeButton().should('be.visible').click()
         homePage.getAddEmployeeTitle().should('be.visible').invoke('text').then((addEmployeeTitle) => {
             expect(addEmployeeTitle.trim()).to.eq('Add Employee')
         })
-        homePage.getFirstNameInput().type(firstName)
-        homePage.getLastNameInput().type(lastName)
-        homePage.getDependentsInput().type(dependants)
+        homePage.getAddFirstNameInput().type(firstName)
+        homePage.getAddLastNameInput().type(lastName)
+        homePage.getAddDependentsInput().type(dependants)
         cy.intercept('POST', '/Prod/api/employees').as('addEmployeeAPI')
         homePage.getAddButton().click()
         cy.wait('@addEmployeeAPI').then((interception) => {
@@ -39,17 +39,61 @@ describe('CRUD test', { tags: ['@smoke'], cases: [1] }, () => {
         cy.get('@uniqueEmployeeID').then((uniqueEmployeeID => {
             homePage.getEmployeeRow(uniqueEmployeeID).should('exist').and('be.visible')
             cy.readFile('cypress/fixtures/shared_data.json').then((data) => {
-                data.production.CRUD = {
-                    employeeId: uniqueEmployeeID,
-                };
-                cy.writeFile('cypress/fixtures/sharedData.json', data);
+                let testD = (env === 'stage') ? data.stage.CRUD : data.production.CRUD
+                testD.employeeId = uniqueEmployeeID
+                cy.writeFile('cypress/fixtures/shared_data.json', data);
             });
         }))
     })
-    it('Edit employee', { cases: [1] }, () => {
+    it('Edit employee', { cases: [3] }, () => {
         cy.readFile('cypress/fixtures/shared_data.json').then((data) => {
-            const id = data.production.CRUD.employeeId;
-            cy.log(`ID: ${id}`);
+            let testD = (env === 'stage') ? data.stage.CRUD : data.production.CRUD
+            cy.wrap(testD.employeeId).as('employeeId')
         });
+        cy.get('@employeeId').then((createdEmployeeID) => {
+            homePage.getEmployeeEditButton(createdEmployeeID).should('exist').click()
+            //The next assertion will be commented until BUG-0006 is solved
+            // homePage.getAddEmployeeTitle().should('be.visible').invoke('text').then((updateEmployeeTitle) => {
+            //     expect(updateEmployeeTitle.trim()).to.eq('Update Employee')
+            // })
+            homePage.getUpdateFirstNameInput().clear().type(firstName + 'Edited')
+            homePage.getUpdateLastNameInput().clear().type(lastName + 'Edited')
+            homePage.getUpdateDependentsInput().clear().type(parseInt(dependants) + 1)
+            cy.intercept('PUT', '/Prod/api/employees').as('updateEmployeeAPI')
+            homePage.getUpdateButton().click()
+            cy.wait('@updateEmployeeAPI').then((interception) => {
+                expect(interception.response.statusCode).to.eq(200);
+                expect(interception.response.body.dependants).to.eq(parseInt(dependants)+1)
+                expect(interception.response.body.firstName).to.eq(firstName + 'Edited');
+                expect(interception.response.body.lastName).to.eq(lastName + 'Edited');
+                expect(interception.response.body.id).to.eq(createdEmployeeID)
+            })
+            homePage.getEmployeeRow(createdEmployeeID).find('td:nth-child(2)').invoke('text').should('eq',firstName + 'Edited')
+            homePage.getEmployeeRow(createdEmployeeID).find('td:nth-child(3)').invoke('text').should('eq',lastName + 'Edited')
+            homePage.getEmployeeRow(createdEmployeeID).find('td:nth-child(4)').invoke('text').should('include',parseInt(dependants)+1)
+
+        })
+    })
+    it('Delete employee', { cases: [4] }, () => {
+        cy.readFile('cypress/fixtures/shared_data.json').then((data) => {
+            let testD = (env === 'stage') ? data.stage.CRUD : data.production.CRUD
+            cy.wrap(testD.employeeId).as('employeeId')
+        });
+        cy.get('@employeeId').then((createdEmployeeID) => {
+            homePage.getEmployeeDeleteButton(createdEmployeeID).should('exist').click()
+            homePage.getDeleteEmployeeTitle().should('be.visible').invoke('text').then((updateEmployeeTitle) => {
+                expect(updateEmployeeTitle.trim()).to.eq(`Delete Employee`)
+            })
+            homePage.getDeleteEmployeeMesage().should('be.visible').invoke('text').then((adivsoryMessage)=>{
+                expect(homePage.normalizeWhitespace(adivsoryMessage)).to.eq(`Delete employee record for ${firstName}Edited ${lastName}Edited?`)
+            })
+            cy.intercept('DELETE',`Prod/api/employees/${createdEmployeeID}`).as('deleteAPI')
+            homePage.getDeleteEmployeeButton().should('be.visible').click()
+            cy.wait('@deleteAPI').then((interception) => {
+                expect(interception.response.statusCode).to.eq(200);
+            })
+            homePage.getEmployeeRow(createdEmployeeID).should('not.exist')
+
+        })
     })
 })
--- cypress/support/Pages/homePage.js
+++ cypress/support/Pages/homePage.js
@@ -6,23 +6,59 @@ class HomePage {
         return cy.get('button[id="add"]', { timeout: 15000 })
     }
     getEmployeeRow(employeeID) {
-        return cy.get(`#employeesTable tbody tr:contains("${employeeID}")`)
+        return cy.get(`#employeesTable tbody tr:contains("${employeeID}")`, { timeout: 15000 })
+    }
+    getEmployeeEditButton(employeeID) {
+        return cy.get(`#employeesTable tbody tr:contains("${employeeID}") i.fas.fa-edit`, { timeout: 15000 })
+    }
+    getEmployeeDeleteButton(employeeID) {
+        return cy.get(`#employeesTable tbody tr:contains("${employeeID}") i.fas.fa-times`, { timeout: 15000 })
     }
     //Add Employee selectors
     getAddEmployeeTitle() {
         return cy.get('.modal-content h5:visible', { timeout: 15000 })
     } 
-    getFirstNameInput() {
+    getAddFirstNameInput() {
         return cy.get('input#firstName', { timeout: 15000 })
     }
-    getLastNameInput() {
+    getAddLastNameInput() {
         return cy.get('input#lastName', { timeout: 15000 })
     }
-    getDependentsInput() {
+    getAddDependentsInput() {
         return cy.get('input#dependants', { timeout: 15000 })
     }
     getAddButton() {
         return cy.get('button#addEmployee', { timeout: 15000 })
+    }
+    //Update employee selectors
+    getUpdateEmployeeTitle() {
+        return cy.get('.modal-content h5:visible', { timeout: 15000 })
+    } 
+    getUpdateFirstNameInput() {
+        return cy.get('input#firstName', { timeout: 15000 })
+    }
+    getUpdateLastNameInput() {
+        return cy.get('input#lastName', { timeout: 15000 })
+    }
+    getUpdateDependentsInput() {
+        return cy.get('input#dependants', { timeout: 15000 })
+    }
+    getUpdateButton() {
+        return cy.get('button#updateEmployee', { timeout: 15000 })
+    }
+    //Delete employee selectors
+    getDeleteEmployeeTitle() {
+        return cy.get('.modal-content h5:visible', { timeout: 15000 })
+    } 
+    getDeleteEmployeeMesage() {
+        return cy.get('.modal-content div[class^="col-sm"]:visible', { timeout: 15000 })
+    }
+    getDeleteEmployeeButton() {
+        return cy.get('button#deleteEmployee', { timeout: 15000 })
+    }
+    //Functions
+    normalizeWhitespace(str) {
+        return str.replace(/\s+/g, ' ').trim();
     }
 }
 export default HomePage
