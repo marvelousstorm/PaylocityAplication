450a15924049cfd21ca742bdf9748468b9e91330
marvelousstorm <brian_vinn@hotmail.com>
2025-05-20 18:45:31 -0600

    Update cypress.config-stage.js

--- cypress.config-stage.js
+++ cypress.config-stage.js
@@ -12,10 +12,11 @@ module.exports = defineConfig({
     },
     env: {
       e2e: true,
-      platform: 'stage',
-      baseURL: 'https://www.saucedemo.com/',
-      username: 'standard_user',
-      password: 'secret_sauce'
+      platform: 'production',
+      baseURL: 'https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/',
+      username: 'TestUser747',
+      password: 'gQ;)DE3[9qFJ',
+      authorizationCode: "Basic VGVzdFVzZXI3NDc6Z1E7KURFM1s5cUZK"
     },
   },
 });
